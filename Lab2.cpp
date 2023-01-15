875c78d5db901814e3c8390bc1f3fb1fb103db29
f33dir <arseniy.tarasov@gmail.com>
2023-01-15 21:13:33 +0300

    Initial commit

---
+++ Lab2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include "AppWindow.h"
+#include "Examples.h"
+#include "Functions.h"
+#include "imgui.h"
+#include "Texture.h"
+
+
+class Window: public AppWindow
+{
+public:
+	Window(SDL_Point windowSize):
+		AppWindow(windowSize),
+		texture(GetRenderer(), windowSize)
+	{
+	}
+	
+	void Render() override
+	{
+		texture.Render();
+	}
+	
+	void RenderGui() override
+	{
+		ImGui::Begin("MyWindow");
+		if (ImGui::Button("Example 1"))
+			Example1(texture);
+		if (ImGui::Button("Example 2"))
+			Example2(texture);
+		if (ImGui::Button("Example 3"))
+			Example3(texture);
+		if (ImGui::Button("Example 4"))
+			Example4(texture);
+		ImGui::End();
+	}
+	
+	void ProcessEvent(const SDL_Event& e) override
+	{
+		
+	}
+	
+	Texture texture;
+};
+
+
+int main(int argc, char** argv)
+{
+	Window window({800, 600});
+	
+	window.Show();
+	
+	return 0;
+}
